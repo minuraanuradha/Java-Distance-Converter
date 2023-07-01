a1a0a47de444587eb98af0e5c1c310d5a6c384a6
Minura Anuradha <112975973+minuraanuradha@users.noreply.github.com>
2023-07-01 19:36:08 +0530

    Add constants for the fixed parameters Step_02

--- DistanceConverter.java
+++ DistanceConverter.java
@@ -1,14 +1,25 @@
 class DistanceConverter 
 {
-    public static void main(String[] args) 
-    {
-        int miles = 26;
-        int yards = 385 ;
+        int miles;
+        int yards;
         double kilometers;
 
-        kilometers = (miles * 1.609 + yards / 1760.0 * 1.609);
-        System.out.println("The Distnce is : " + kilometers + " km");
+    public DistanceConverter ( int txtmiles ,int txtyards )        
+    {
+        miles = txtmiles;
+        yards = txtyards;            
+    }
 
+    public void conversion()
+    {
+        kilometers = (miles * 1.609 + yards / 1760.0 * 1.609);
+    }
+    
+    public static void main(String[] args) 
+    {
+        DistanceConverter mycon01 = new DistanceConverter (26,385);
+        mycon01.conversion();
+        System.out.println("The Distnce is : " + mycon01.kilometers + " km");
     }
 }
-    
+
